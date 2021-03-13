930bb1f32686587fa9e67aea42c5bc6c135e47cf
eddonecoff <ethan.donecoff@duke.edu>
2021-03-13 09:38:23 -0500

    finish longTime function

--- hw4.py
+++ hw4.py
@@ -5,39 +5,50 @@ NetID(s):
 Date:
 """
 
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 def longTime(A):
-    return (domEig, domVec)
+	w,v = np.linalg.eig(A)
+	domEig = max(w)
+	domVec = v[np.where(w == domEig)]
+	return(domEig, domVec)
 
 def getAk(A,k):
-    return AK
+	return AK
 
 def error(uCurr, uLong):
-    return err
+	return err
 
 def normalize(v):
-    return v
+	return v
 
 def simulate(A,k,u0):
-    return sim
+	return sim
 
 """
 main function
 """
+
+
+def main():
+
+	A = np.array([[2,1],[1,1]])
+	print(longTime(A))
+
+	# Scenario A: Frogs
+	frogMat = [[0.0, 0.0, 3.0,  8.0], \
+			   [0.4, 0.0, 0.0,  0.0], \
+			   [0.0, 0.5, 0.0,  0.0], \
+			   [0.0, 0.0, 0.25, 0.0]]
+	frogInit = [[0.0],[0.0],[0.0],[250.0]]
+
+	# Scenario B: Owls
+	owlMat = [[0.2,  0.1,  0.4,  1/3], \
+			  [0.4,  0.4,  0.2,  1/3], \
+			  [0.2,  0.3,  0.2,  1/3], \
+			  [0.01, 0.01, 0.01, 1.5]]
+	owlInit = [[100.0],[100.0],[0.0],[0.0]]
+
 if __name__ == '__main__':
-
-    # Scenario A: Frogs
-    frogMat = [[0.0, 0.0, 3.0,  8.0], \
-               [0.4, 0.0, 0.0,  0.0], \
-               [0.0, 0.5, 0.0,  0.0], \
-               [0.0, 0.0, 0.25, 0.0]]
-    frogInit = [[0.0],[0.0],[0.0],[250.0]]
-
-    # Scenario B: Owls
-    owlMat = [[0.2,  0.1,  0.4,  1/3], \
-              [0.4,  0.4,  0.2,  1/3], \
-              [0.2,  0.3,  0.2,  1/3], \
-              [0.01, 0.01, 0.01, 1.5]]
-    owlInit = [[100.0],[100.0],[0.0],[0.0]]
+	main()
